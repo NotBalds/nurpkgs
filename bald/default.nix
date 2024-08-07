f8d4bdce7cd88d6d70a81728c4818792380292b6
ProggerX <x@proggers.ru>
2024-08-07 16:24:42 +0300

    bald: init at 1.0.0

---
+++ bald/default.nix
@@ -0,0 +1,14 @@
+{ stdenv, fetchFromGitHub, gnumake, gcc }:
+stdenv.mkDerivation {
+	pname = "bald";
+	version = "v1.0.0";
+	src = fetchFromGitHub {
+		owner = "NotBalds";
+		repo = "bald";
+		rev = "7a31e3c6b25788dc9443f5a726224c15ffa9df16";
+		hash = "sha256-eEohxzltnD7Y2a6SsxpEXgIBcSNZ3tIP4vQrC5o/Zqo=";
+	};
+	nativeBuildInputs = [ gnumake gcc ];
+	buildPhase = "make build";
+	installPhase = "mkdir -p $out/bin && cp out $out/bin/bald";
+}
